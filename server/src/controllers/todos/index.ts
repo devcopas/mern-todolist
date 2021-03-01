f91570c2396643cabe314779dec8b8c4f76ef1d6
devcopas <borjutimothy@gmail.com>
2021-03-01 18:46:36 +0700

    create addTodo API controller

--- server/src/controllers/todos/index.ts
+++ server/src/controllers/todos/index.ts
@@ -10,10 +10,32 @@ export const getTodos = async (req: Request, res: Response) => {
 
 export const getTodo = async (req: Request, res: Response) => {
 	await TodoModel.findById(req.params.id, (err, result) => {
-		if (err) {
-			res.status(400).json({ error: err })
-		} else {
-			res.status(200).json({ result })
-		}
+		if (err) res.status(400).json({ error: err })
+		else res.status(200).json({ result })
 	})
+}
+
+export const addTodo = async (req: Request, res: Response) => {
+	const body: Pick<Todo, 'title' | 'status'> = req.body;
+
+	if (!body.title || !body.status) {
+		res.status(401).json({
+			status: 401,
+			errorMessage: `ValidationError: Todo Validation failed:title:${body.title},status:${body.status}`
+		});
+		return
+	}
+
+	const newTodoModel = new TodoModel({
+		title: body.title,
+		status: body.status
+	});
+	const newTodo = await newTodoModel.save();
+	const updatedAllTodosAfterSave = await TodoModel.find()
+
+	res.status(201).json({
+		message: 'Todo successfully added!',
+		addedTodo: newTodo,
+		allTodosAfterAddition: updatedAllTodosAfterSave
+	});
 }
