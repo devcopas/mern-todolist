df3ae731c5252fefb23dfe596d4f446029a6aade
devcopas <borjutimothy@gmail.com>
2021-03-01 18:22:52 +0700

    create getTodoAPI controller

--- server/src/controllers/todos/index.ts
+++ server/src/controllers/todos/index.ts
@@ -5,5 +5,15 @@ import { Todo } from '../../types/todo';
 
 export const getTodos = async (req: Request, res: Response) => {
 	const todos: Todo[] = await TodoModel.find();
-	res.status(200).json({ Todo });
+	res.status(200).json({ todos });
+}
+
+export const getTodo = async (req: Request, res: Response) => {
+	await TodoModel.findById(req.params.id, (err, result) => {
+		if (err) {
+			res.status(400).json({ error: err })
+		} else {
+			res.status(200).json({ result })
+		}
+	})
 }
