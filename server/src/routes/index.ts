e55eec804a3757592b6ed40c43842ede66bc309e
devcopas <borjutimothy@gmail.com>
2021-03-01 18:12:41 +0700

    create api endpoints

---
+++ server/src/routes/index.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('api/todos', getTodos);
+router.get('api/todo/:id', getTodo);
+router.post('api/add-todo', addTodo);
+router.put('api/update-todo/:id', updateTodo);
+router.delete('api/delete-todo/:id', deleleTodo);
+
+export default router;
